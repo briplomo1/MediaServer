fb4ef36fae0d7ba081b1583ecaa9c11cf0109eea
bryan <briplomo@gmail.com>
2024-08-27 01:30:56 -0500

    working window open

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,11 +2,29 @@
 //
 
 #include "Server.h"
+#include <stdio.h>
+#include<GLFW/glfw3.h>
 
 using namespace std;
 
-int main()
-{
+int main(int argc, const char** argv) {
 	cout << "Hello CMake." << endl;
+	GLFWwindow* window;
+	if (!glfwInit()) {
+		printf("Unable to init glfw!\n");
+		return 1;
+	}
+	
+	window = glfwCreateWindow(640, 480, "Hello!", NULL, NULL);
+	if (!window) {
+		printf("Unable to open window!\n");
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwWaitEvents();
+	}
 	return 0;
 }
